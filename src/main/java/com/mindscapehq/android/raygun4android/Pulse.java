fe677e1b21209b9025ef58ef27d0263964006f60
Jason Fauchelle <jfauchelle@raygun.io>
2016-05-27 10:26:04 +1200

    Activity lifecycle logic

--- src/main/java/com/mindscapehq/android/raygun4android/Pulse.java
+++ src/main/java/com/mindscapehq/android/raygun4android/Pulse.java
@@ -37,36 +37,72 @@ public class Pulse implements ActivityLifecycleCallbacks {
 
   @Override
   public void onActivityCreated(Activity activity, Bundle bundle) {
+    if(_currentActivity == null) {
+      RaygunClient.SendPulseEvent("session_start");
+    }
+
+    if(activity != _currentActivity) {
+      _currentActivity = activity;
+      _startTime = System.nanoTime();
+    }
     System.out.println("CREATED");
   }
 
   @Override
   public void onActivityStarted(Activity activity) {
+    if(_currentActivity == null) {
+      RaygunClient.SendPulseEvent("session_start");
+    }
+
+    if(activity != _currentActivity) {
+      _currentActivity = activity;
+      _startTime = System.nanoTime();
+    }
     System.out.println("STARTED");
   }
 
   @Override
   public void onActivityResumed(Activity activity) {
+    if(_currentActivity == null) {
+      RaygunClient.SendPulseEvent("session_start");
+    }
+
+    String activityName = getActivityName(activity);
+    double duration = 0;
+    if(activity == _currentActivity) {
+      duration = System.nanoTime() - _startTime;
+    }
+    _currentActivity = activity;
+
+    RaygunClient.SendPulsePageTimingEvent(activityName, duration);
     System.out.println("RESUMED");
   }
 
   @Override
   public void onActivityPaused(Activity activity) {
-    System.out.println("PAUSED");
+    //System.out.println("PAUSED");
   }
 
   @Override
   public void onActivityStopped(Activity activity) {
+    if(activity == _currentActivity) {
+      _currentActivity = null;
+      RaygunClient.SendPulseEvent("session_start");
+    }
     System.out.println("STOPPED");
   }
 
   @Override
   public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
-    System.out.println("SAVE");
+    //System.out.println("SAVE");
   }
 
   @Override
   public void onActivityDestroyed(Activity activity) {
-    System.out.println("DESTROYED");
+    //System.out.println("DESTROYED");
+  }
+
+  private static String getActivityName(Activity activity) {
+    return activity.getClass().getName();
   }
 }
