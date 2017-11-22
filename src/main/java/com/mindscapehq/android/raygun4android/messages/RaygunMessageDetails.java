f6ccad92e3b5a534df8b5319dae8e4250f820d7d
Mitchell Duncan <mitchell@raygun.com>
2017-11-22 16:03:27 +1300

    Expose the grouping key and the NetworkInfo object in the message details.

--- src/main/java/com/mindscapehq/android/raygun4android/messages/RaygunMessageDetails.java
+++ src/main/java/com/mindscapehq/android/raygun4android/messages/RaygunMessageDetails.java
@@ -18,91 +18,49 @@ public class RaygunMessageDetails {
   private RaygunUserContext user;
   private NetworkInfo request;
 
-  public void setMachineName(String machineName) {
-    this.machineName = machineName;
-  }
+  // Machine Name
+  public String getMachineName() { return machineName; }
+  public void setMachineName(String machineName) { this.machineName = machineName; }
 
-  public String getMachineName() {
-    return machineName;
-  }
+  // Version
+  public String getVersion() { return version; }
+  public void setVersion(String version) { this.version = version; }
 
-  public String getVersion() {
-    return version;
-  }
+  // Error
+  public RaygunErrorMessage getError() { return error; }
+  public void setError(RaygunErrorMessage error) { this.error = error; }
 
-  public void setVersion(String version) {
-    this.version = version;
-  }
+  // Environment
+  public RaygunEnvironmentMessage getEnvironment() { return environment; }
+  public void setEnvironment(RaygunEnvironmentMessage environment) { this.environment = environment; }
 
-  public RaygunErrorMessage getError() {
-    return error;
-  }
+  // Client
+  public RaygunClientMessage getClient() { return client; }
+  public void setClient(RaygunClientMessage client) { this.client = client; }
 
-  public void setError(RaygunErrorMessage error) {
-    this.error = error;
-  }
+  // Tags
+  public List getTags() { return tags; }
+  public void setTags(List tags) { this.tags = tags; }
 
-  public RaygunEnvironmentMessage getEnvironment() {
-    return environment;
-  }
+  // Custom Data
+  public Map getUserCustomData() { return userCustomData; }
+  public void setUserCustomData(Map userCustomData) { this.userCustomData = userCustomData; }
 
-  public void setEnvironment(RaygunEnvironmentMessage environment) {
-    this.environment = environment;
-  }
+  // App Context
+  public RaygunAppContext getAppContext() { return context; }
+  public void setAppContext(String identifier) { this.context = new RaygunAppContext(identifier); }
 
-  public RaygunClientMessage getClient() {
-    return client;
-  }
+  // User
+  public RaygunUserContext getUserContext() { return user; }
+  public void setUserContext(Context context) { this.user = new RaygunUserContext(context); }
+  public void setUserContext(String user) { this.user = new RaygunUserContext(user); }
+  public void setUserContext(RaygunUserInfo userInfo, Context context) { this.user = new RaygunUserContext(userInfo, context); }
 
-  public void setClient(RaygunClientMessage client) {
-    this.client = client;
-  }
+  // Network Info
+  public NetworkInfo getNetworkInfo(Context context) { return request; }
+  public void setNetworkInfo(Context context) { this.request = new NetworkInfo(context); }
 
-  public List getTags() {
-    return tags;
-  }
-
-  public void setTags(List tags) {
-    this.tags = tags;
-  }
-
-  public Map getUserCustomData() {
-    return userCustomData;
-  }
-
-  public void setUserCustomData(Map userCustomData) {
-    this.userCustomData = userCustomData;
-  }
-
-  public RaygunAppContext getAppContext() {
-    return context;
-  }
-
-  public void setAppContext(String identifier) {
-    this.context = new RaygunAppContext(identifier);
-  }
-
-  public RaygunUserContext getUserContext() {
-    return user;
-  }
-
-  public void setUserContext(Context context) {
-    this.user = new RaygunUserContext(context);
-  }
-
-  public void setUserContext(String user) {
-    this.user = new RaygunUserContext(user);
-  }
-
-  public void setUserContext(RaygunUserInfo userInfo, Context context) {
-    this.user = new RaygunUserContext(userInfo, context);
-  }
-
-  public void setNetworkInfo(Context context) {
-    this.request = new NetworkInfo(context);
-  }
-
-  public void setGroupingKey(String groupingKey) {
-    this.groupingKey = groupingKey;
-  }
+  // Grouping Key
+  public String getGroupingKey() { return groupingKey; }
+  public void setGroupingKey(String groupingKey) { this.groupingKey = groupingKey; }
 }
