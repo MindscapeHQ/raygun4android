43a8ec0985ea1cc7e369cc15bc8be8e8d0fba13b
Kai Koenig <kai@ventego-creative.co.nz>
2018-09-27 12:30:49 +1200

    Added TODO for weird functionality to check

--- provider/src/main/java/com/raygun/raygun4android/OnPackageReplaced.java
+++ provider/src/main/java/com/raygun/raygun4android/OnPackageReplaced.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 
 public class OnPackageReplaced extends BroadcastReceiver {
+  // TODO Kai - this seems to be used nowhere - check if a) this is needed and b) this checks for every single app-update on a system
   @Override
   public void onReceive(Context context, Intent intent) {
       RaygunClient.closePostService();
