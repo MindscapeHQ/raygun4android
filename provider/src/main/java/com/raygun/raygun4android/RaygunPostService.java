5aacc80f5c2c4619111f96f33d46953eb43c4738
Kai Koenig <kai@ventego-creative.co.nz>
2018-09-28 12:11:30 +1200

    Changed Service to IntentService with associated changes to RaygunClient

--- provider/src/main/java/com/raygun/raygun4android/RaygunPostService.java
+++ provider/src/main/java/com/raygun/raygun4android/RaygunPostService.java
@@ -1,6 +1,6 @@
 package com.raygun.raygun4android;
 
-import android.app.Service;
+import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
@@ -16,101 +16,83 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class RaygunPostService extends Service {
-  private int tCount = 0;
-  private Intent intent;
+public class RaygunPostService extends IntentService {
 
-  @Override
-  public int onStartCommand(Intent intent, int flags, int startId) {
-    // the intent should never be null:
-    // https://developer.android.com/reference/android/app/Service#START_NOT_STICKY
-    // but somehow it is
-    if (intent == null || intent.getExtras() == null) {
-      return START_NOT_STICKY;
+    private Intent intent;
+
+    public RaygunPostService() {
+        super("RaygunPostService");
     }
 
-    this.intent = intent;
-    final Bundle bundle = intent.getExtras();
+    @Override
+    public void onHandleIntent(Intent intent) {
 
-    Thread t = new Thread(new Runnable() {
-      @Override
-      public void run() {
-        String message = (String) bundle.get("msg");
-        String apiKey = (String) bundle.get("apikey");
-        boolean isPulse = ((String) bundle.get("isPulse")).equals("True");
-        boolean hasInternet = hasInternetConnection();
+        if (intent != null && intent.getExtras() != null) {
+            this.intent = intent;
 
-        if (isPulse && hasInternet) {
-          RaygunClient.postPulseMessage(apiKey, message);
-        } else if (!isPulse && hasInternet) {
-          RaygunClient.post(apiKey, message);
-        } else if (!isPulse && !hasInternet) {
-          synchronized (this) {
-            int file = 0;
-            ArrayList<File> files = new ArrayList<File>(Arrays.asList(getCacheDir().listFiles()));
-            if (files != null) {
-              for (File f : files) {
-                String fileName = Integer.toString(file) + ".raygun";
-                if (RaygunClient.getExtension(f.getName()).equals("raygun") && !f.getName().equals(fileName)) {
-                  break;
+            final Bundle bundle = intent.getExtras();
+
+            String message = bundle.getString("msg");
+            String apiKey = bundle.getString("apikey");
+            boolean isPulse = bundle.getBoolean("isPulse");
+            boolean hasInternet = hasInternetConnection();
+
+            if (isPulse && hasInternet) {
+                RaygunClient.postPulseMessage(apiKey, message);
+            } else if (!isPulse && hasInternet) {
+                RaygunClient.post(apiKey, message);
+            } else if (!isPulse && !hasInternet) {
+                synchronized (this) {
+                    int file = 0;
+                    ArrayList<File> files = new ArrayList<File>(Arrays.asList(getCacheDir().listFiles()));
+                    if (files != null) {
+                        for (File f : files) {
+                            String fileName = Integer.toString(file) + ".raygun";
+                            if (RaygunClient.getExtension(f.getName()).equals("raygun") && !f.getName().equals(fileName)) {
+                                break;
+                            } else if (file < 64) {
+                                file++;
+                            } else {
+                                files.get(0).delete();
+                            }
+                        }
+                    }
+                    File fn = new File(getCacheDir(), Integer.toString(file) + ".raygun");
+                    try {
+                        MessageApiKey messageApiKey = new MessageApiKey(apiKey, message);
+                        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fn));
+                        out.writeObject(messageApiKey);
+                        out.close();
+                    } catch (FileNotFoundException e) {
+                        RaygunLogger.e("Error creating file when caching message to filesystem - " + e.getMessage());
+                    } catch (IOException e) {
+                        RaygunLogger.e("Error writing message to filesystem - " + e.getMessage());
+                    }
                 }
-                else if (file < 64) {
-                  file++;
-                }
-                else {
-                  files.get(0).delete();
-                }
-              }
             }
-            File fn = new File(getCacheDir(), Integer.toString(file) + ".raygun");
-            try {
-              MessageApiKey messageApiKey = new MessageApiKey(apiKey, message);
-              ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fn));
-              out.writeObject(messageApiKey);
-              out.close();
-            }
-            catch (FileNotFoundException e) {
-              RaygunLogger.e("Error creating file when caching message to filesystem - " + e.getMessage());
-            }
-            catch (IOException e) {
-              RaygunLogger.e("Error writing message to filesystem - " + e.getMessage());
-            }
-          }
         }
-        tCount--;
-        if (tCount == 0) {
-          stopSelf();
-        }
-      }
-    });
-
-    t.setDaemon(true);
-    tCount++;
-    t.start();
-
-    return START_NOT_STICKY;
-  }
-
-  private boolean hasInternetConnection() {
-  
-    ConnectivityManager cm = (ConnectivityManager) this.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-
-    if (cm != null) {
-      NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-      return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
-    
-    return false;
-  }
 
-  @Override
-  public void onDestroy() {
-    super.onDestroy();
-    stopService(intent);
-  }
+    private boolean hasInternetConnection() {
 
-  @Override
-  public IBinder onBind(Intent intent) {
-    return null;
-  }
+        ConnectivityManager cm = (ConnectivityManager) this.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (cm != null) {
+            NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+            return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopSelf();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
