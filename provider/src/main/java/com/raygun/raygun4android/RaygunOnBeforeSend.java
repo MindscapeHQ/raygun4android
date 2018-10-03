bfa5d3eca5512cd86debc68430853822598d9217
Kai Koenig <kai@ventego-creative.co.nz>
2018-10-03 17:22:16 +1300

    Removed deprecated method

--- provider/src/main/java/com/raygun/raygun4android/RaygunOnBeforeSend.java
+++ provider/src/main/java/com/raygun/raygun4android/RaygunOnBeforeSend.java
@@ -3,10 +3,5 @@ package com.raygun.raygun4android;
 import com.raygun.raygun4android.messages.RaygunMessage;
 
 public interface RaygunOnBeforeSend {
-  RaygunMessage onBeforeSend(RaygunMessage message);
-
-  /**
-   * @deprecated As of release 3.0.0, replaced by {@link #onBeforeSend(RaygunMessage)}
-   */
-  @Deprecated RaygunMessage OnBeforeSend(RaygunMessage message);
+    RaygunMessage onBeforeSend(RaygunMessage message);
 }
