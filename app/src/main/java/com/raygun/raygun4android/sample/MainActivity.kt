7a8f7d9844a58e183735fe0c7cee512cfe7532c8
Kai Koenig <kai@ventego-creative.co.nz>
2018-10-14 16:00:33 +1300

    Added more buttons to the sample app to cater for different testing and setup/usage flows

--- app/src/main/java/com/raygun/raygun4android/sample/MainActivity.kt
+++ app/src/main/java/com/raygun/raygun4android/sample/MainActivity.kt
@@ -3,7 +3,7 @@ package com.raygun.raygun4android.sample
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.raygun.raygun4android.RaygunClient
-import com.raygun.raygun4android.messages.RaygunUserInfo
+import com.raygun.raygun4android.messages.shared.RaygunUserInfo
 import android.widget.Button
 
 
@@ -17,35 +17,54 @@ class MainActivity : AppCompatActivity() {
         initialCustomData.put("firstkey", "firstvalue")
 
         RaygunClient.init(application)             // This sets up the client with the API key as provided in your AndroidManifest.xml
-        RaygunClient.attachExceptionHandler()      // This attaches a pre-made exception handler to catch all uncaught exceptions, and send them to Raygun
-        RaygunClient.attachRUM(this)
+        RaygunClient.enableCrashReporting()
+        RaygunClient.enableRUM(this)
 
         RaygunClient.setUserCustomData(initialCustomData)
 
-        val button = findViewById(R.id.button) as Button
+        val button_send = findViewById(R.id.button_send) as Button
+        val button_crash = findViewById(R.id.button_crash) as Button
+        val button_set_user_anon = findViewById(R.id.button_set_user_anon) as Button
+        val button_set_user_A = findViewById(R.id.button_set_user_A) as Button
+        val button_set_user_B = findViewById(R.id.button_set_user_B) as Button
 
-        button.setOnClickListener{
-
-            var user = RaygunUserInfo()
-            user.fullName = "User Name"
-            user.firstName = "User"
-            user.email = "a@b.com"
-            user.uuid = "a uuid"
-            user.setAnonymous(false)
-
+        button_send.setOnClickListener {
             val tw = HashMap<String,String>()
-
             tw.put("secondkey", "secondvalue")
 
-            // Example 1: Manual exception creation & sending
+            // Manual exception creation & sending
             RaygunClient.send(Exception("Congratulations, you have sent errors with Raygun4Android"), null, tw)
+        }
 
-            // Example 2: Manual exception creation & sending
+        button_crash.setOnClickListener {
+            // A real exception will be thrown here, which will be caught & sent by RaygunClient
+            val i = 3 / 0
+        }
+
+        button_set_user_anon.setOnClickListener {
+            var user = RaygunUserInfo()
+            user.setAnonymous(true)
             RaygunClient.setUser(user)
-            RaygunClient.send(Exception("Congratulations, you have sent errors with Raygun4Android, now with a user"), null, tw)
+        }
 
-            // Example 3: A real exception will be thrown here, which will be caught & sent by RaygunClient
-            // val i = 3 / 0
+        button_set_user_A.setOnClickListener {
+            var user = RaygunUserInfo()
+            user.identifier = "superuser1"
+            user.fullName = "User Name A"
+            user.firstName = "User A"
+            user.email = "a@b.com"
+            user.setAnonymous(false)
+            RaygunClient.setUser(user)
+        }
+
+        button_set_user_B.setOnClickListener{
+            var user = RaygunUserInfo()
+            user.identifier = "superuser2"
+            user.fullName = "User Name B"
+            user.firstName = "User B"
+            user.email = "b@c.com"
+            user.setAnonymous(false)
+            RaygunClient.setUser(user)
         }
     }
 }
