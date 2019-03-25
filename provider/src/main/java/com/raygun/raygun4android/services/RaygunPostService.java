599715dfca25a69df05e3808bd3e16a0de4a32fc
Kai Koenig <kai@ventego-creative.co.nz>
2019-03-25 17:32:02 +1300

    Fixing the visibility of the abstract RaygunPostService

--- provider/src/main/java/com/raygun/raygun4android/services/RaygunPostService.java
+++ provider/src/main/java/com/raygun/raygun4android/services/RaygunPostService.java
@@ -4,8 +4,16 @@ import android.support.v4.app.JobIntentService;
 
 import com.raygun.raygun4android.RaygunLogger;
 
-abstract class RaygunPostService extends JobIntentService {
-
+/**
+ * A JobIntentService that can validate Raygun API keys
+ */
+public abstract class RaygunPostService extends JobIntentService {
+    /**
+     * Validation to check if an API key has been supplied to the service
+     *
+     * @param apiKey      The API key of the app to deliver to
+     * @return true or false
+     */
     protected static Boolean validateApiKey(String apiKey) {
         if (apiKey.length() == 0) {
             RaygunLogger.e("API key is empty, nothing will be logged or reported");
