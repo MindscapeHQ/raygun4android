250e229cd6f1ce4b4b4888c81a14da390afb6bb1
Kai Koenig <kai@ventego-creative.co.nz>
2019-03-26 18:43:26 +1300

    Some changes arising from linting the sample app.

--- app/src/main/java/com/raygun/raygun4android/sample/MainActivity.kt
+++ app/src/main/java/com/raygun/raygun4android/sample/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val initialCustomData = HashMap<String,String>()
-        initialCustomData.put("firstkey", "firstvalue")
+        initialCustomData["firstkey"] = "firstvalue"
 
         // This sets up the client with the API key as provided in your AndroidManifest.xml
         RaygunClient.init(applicationContext as Application)
@@ -26,50 +26,50 @@ class MainActivity : AppCompatActivity() {
 
         RaygunClient.setUserCustomData(initialCustomData)
 
-        val button_send = findViewById(R.id.button_send) as Button
-        val button_crash = findViewById(R.id.button_crash) as Button
-        val button_set_user_anon = findViewById(R.id.button_set_user_anon) as Button
-        val button_set_user_A = findViewById(R.id.button_set_user_A) as Button
-        val button_set_user_B = findViewById(R.id.button_set_user_B) as Button
-        val textView_appVersion = findViewById(R.id.textView_appVersion) as TextView
-        val textView_providerVersion = findViewById(R.id.textView_providerVersion) as TextView
+        val buttonSend = findViewById(R.id.button_send) as Button
+        val buttonCrash = findViewById(R.id.button_crash) as Button
+        val buttonSetUserAnon = findViewById(R.id.button_set_user_anon) as Button
+        val buttonSetUserA = findViewById(R.id.button_set_user_A) as Button
+        val buttonSetUserB = findViewById(R.id.button_set_user_B) as Button
+        val textViewAppVersion = findViewById(R.id.textView_appVersion) as TextView
+        val textViewProviderVersion = findViewById(R.id.textView_providerVersion) as TextView
 
-        button_send.setOnClickListener {
+        buttonSend.setOnClickListener {
             val tw = HashMap<String,String>()
-            tw.put("secondkey", "secondvalue")
+            tw["secondkey"] = "secondvalue"
 
             // Manual exception creation & sending
             RaygunClient.send(Exception("Congratulations, you have sent errors with Raygun4Android"), null, tw)
         }
 
-        button_crash.setOnClickListener {
+        buttonCrash.setOnClickListener {
             // A real exception will be thrown here, which will be caught & sent by RaygunClient
             val i = 3 / 0
             Log.d("Raygun4Android-Sample", "This is here purely so that our division by zero calculation in i gets used and not optimised away in a release build: $i")
         }
 
-        button_set_user_anon.setOnClickListener {
-            var user = RaygunUserInfo()
+        buttonSetUserAnon.setOnClickListener {
+            val user = RaygunUserInfo()
             RaygunClient.setUser(user)
         }
 
-        button_set_user_A.setOnClickListener {
-            var user = RaygunUserInfo("superuser3")
+        buttonSetUserA.setOnClickListener {
+            val user = RaygunUserInfo("superuser3")
             user.fullName = "User Name C"
             user.firstName = "User C"
             user.email = "e@f.com.com"
             RaygunClient.setUser(user)
         }
 
-        button_set_user_B.setOnClickListener{
-            var user = RaygunUserInfo("superuser4")
+        buttonSetUserB.setOnClickListener{
+            val user = RaygunUserInfo("superuser4")
             user.fullName = "User Name D"
             user.firstName = "User D"
             user.email = "g@h.com"
             RaygunClient.setUser(user)
         }
 
-        textView_appVersion.text = "App ${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE} ${BuildConfig.BUILD_TYPE})";
-        textView_providerVersion.text = "Provider ${com.raygun.raygun4android.BuildConfig.VERSION_NAME} (${com.raygun.raygun4android.BuildConfig.VERSION_CODE} ${BuildConfig.BUILD_TYPE})";
+        textViewAppVersion.text = "App ${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE} ${BuildConfig.BUILD_TYPE})"
+        textViewProviderVersion.text = "Provider ${com.raygun.raygun4android.BuildConfig.VERSION_NAME} (${com.raygun.raygun4android.BuildConfig.VERSION_CODE} ${BuildConfig.BUILD_TYPE})"
     }
 }
