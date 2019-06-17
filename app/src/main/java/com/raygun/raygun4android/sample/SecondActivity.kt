6791a1f92423f359ee4a89f6c3b2cc48f4ed2d14
Kai Koenig <kai@ventego-creative.co.nz>
2019-06-17 22:44:18 +0200

    Implemented Second Activity in sample app

--- app/src/main/java/com/raygun/raygun4android/sample/SecondActivity.kt
+++ app/src/main/java/com/raygun/raygun4android/sample/SecondActivity.kt
@@ -1,46 +1,38 @@
 package com.raygun.raygun4android.sample
 
-import android.app.Application
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import com.raygun.raygun4android.RaygunClient
-import com.raygun.raygun4android.messages.shared.RaygunUserInfo
 import android.widget.Button
 import android.widget.TextView
+import com.raygun.raygun4android.RaygunClient
+import com.raygun.raygun4android.messages.shared.RaygunUserInfo
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
 
-        val initialCustomData = HashMap<String,String>()
-        initialCustomData["firstkey"] = "firstvalue"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        // This sets up the client with the API key as provided in your AndroidManifest.xml
-        RaygunClient.init(applicationContext as Application)
-        RaygunClient.enableCrashReporting()
-        RaygunClient.enableRUM(this)
-
-        RaygunClient.setCustomData(initialCustomData)
-
-        val buttonSend = findViewById<Button>(R.id.button_send)
-        val buttonCrash = findViewById<Button>(R.id.button_crash)
-        val buttonHandleException = findViewById<Button>(R.id.button_handleException)
-        val buttonSetUserAnon = findViewById<Button>(R.id.button_set_user_anon)
-        val buttonSetUserA = findViewById<Button>(R.id.button_set_user_A)
-        val buttonSetUserB = findViewById<Button>(R.id.button_set_user_B)
-        val textViewAppVersion = findViewById<TextView>(R.id.textView_appVersion)
-        val textViewProviderVersion = findViewById<TextView>(R.id.textView_providerVersion)
-        val buttonSecondActivity = findViewById<Button>(R.id.button_secondActivity)
+        val buttonSend = findViewById(R.id.button_send) as Button
+        val buttonCrash = findViewById(R.id.button_crash) as Button
+        val buttonHandleException = findViewById(R.id.button_handleException) as Button
+        val buttonSetUserAnon = findViewById(R.id.button_set_user_anon) as Button
+        val buttonSetUserA = findViewById(R.id.button_set_user_A) as Button
+        val buttonSetUserB = findViewById(R.id.button_set_user_B) as Button
+        val textViewAppVersion = findViewById(R.id.textView_appVersion) as TextView
+        val textViewProviderVersion = findViewById(R.id.textView_providerVersion) as TextView
 
         buttonSend.setOnClickListener {
             val tw = HashMap<String,String>()
             tw["secondkey"] = "secondvalue"
 
             // Manual exception creation & sending
-            RaygunClient.send(Exception("Congratulations, you have sent errors with Raygun4Android"), null, tw)
+            RaygunClient.send(Exception("Congratulations, you have sent errors with Raygun4Android from SecondActivity"), null, tw)
         }
 
         buttonCrash.setOnClickListener {
@@ -81,12 +73,11 @@ class MainActivity : AppCompatActivity() {
             RaygunClient.setUser(user)
         }
 
-        buttonSecondActivity.setOnClickListener {
-            startActivity(SecondActivity.getIntent(this@MainActivity))
-        }
-
-
         textViewAppVersion.text = "App ${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE} ${BuildConfig.BUILD_TYPE})"
         textViewProviderVersion.text = "Provider ${com.raygun.raygun4android.BuildConfig.VERSION_NAME} (${com.raygun.raygun4android.BuildConfig.VERSION_CODE} ${BuildConfig.BUILD_TYPE})"
+    }
+
+    companion object {
+        fun getIntent(context: Context): Intent = Intent(context, SecondActivity::class.java)
     }
 }
