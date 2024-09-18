a00b59af8fd784e34ffc2bdc58bfec940522ae56
Kai Koenig <kai@ventego-creative.co.nz>
2024-09-18 23:12:26 +1200

    Mockito and simplistic test

---
+++ provider/src/test/java/com/raygun/raygun4android/RaygunClientTest.kt
@@ -0,0 +1,11 @@
+package com.raygun.raygun4android
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class RaygunClientTest {
+    @Test
+    fun testRaygunClientInit() {
+        assertTrue(5 == 5)
+    }
+}
