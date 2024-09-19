26290e7cb8a13145ff426d643497f676cde82d0f
Kai Koenig <kai@ventego-creative.co.nz>
2024-09-19 22:16:17 +1200

    Tests

--- provider/src/test/java/com/raygun/raygun4android/RaygunClientTest.kt
+++ provider/src/test/java/com/raygun/raygun4android/RaygunClientTest.kt
@@ -1,11 +1,47 @@
 package com.raygun.raygun4android
 
-import org.junit.Assert.assertTrue
+import android.app.Application
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.whenever
+import timber.log.Timber
 
+@RunWith(MockitoJUnitRunner::class)
 class RaygunClientTest {
-    @Test
-    fun testRaygunClientInit() {
-        assertTrue(5 == 5)
+
+    @Before
+    fun setup() {
+        Timber.plant(TestTree())
     }
+
+    @After
+    fun tearDown() {
+        Timber.uprootAll()
+    }
+
+    @Mock
+    private lateinit var mockApplication: Application
+
+    @Test
+    fun initializesWithApplicationAndApiKeyAndVersion() {
+
+        val mockApplication = mock<Application>(Application::class.java)
+        whenever(mockApplication.applicationContext).thenReturn(mockApplication)
+
+        val apiKey = "testApiKey"
+
+        RaygunClient.init(mockApplication, apiKey, "1.0.0")
+
+        assertEquals(apiKey, RaygunClient.getApiKey())
+        assertEquals("1.0.0", RaygunClient.getVersion())
+        assertNotNull(RaygunClient.getApplicationContext())
+    }
+
 }
---
+++ provider/src/test/java/com/raygun/raygun4android/TestTree.kt
@@ -0,0 +1,9 @@
+package com.raygun.raygun4android
+
+import timber.log.Timber
+
+class TestTree : Timber.Tree() {
+    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        // Do nothing
+    }
+}
