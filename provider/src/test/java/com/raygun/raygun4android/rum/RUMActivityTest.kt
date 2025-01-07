81e0446117731d45c0279acbea9a8441f6f3f66d
Miguel Beltran <m@beltran.work>
2025-01-07 16:45:22 +0100

    add RUMActivityTests

--- provider/src/test/java/com/raygun/raygun4android/rum/RUMActivityTest.kt
+++ provider/src/test/java/com/raygun/raygun4android/rum/RUMActivityTest.kt
@@ -1,26 +1,22 @@
 package com.raygun.raygun4android.rum
 
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.raygun.raygun4android.RaygunRUMEventType
 import com.raygun.raygun4android.TestTree
-import junit.framework.TestCase.fail
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.Mockito.anyInt
 import org.mockito.Mockito.anyLong
 import org.mockito.Mockito.mock
-import org.mockito.junit.MockitoJUnitRunner
-import org.mockito.kotlin.any
 import org.mockito.kotlin.eq
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 import timber.log.Timber
 
-@RunWith(MockitoJUnitRunner::class)
-class RUMFragmentTest {
+class RUMActivityTest {
     @Before
     fun setup() {
         Timber.plant(TestTree())
@@ -32,28 +28,30 @@ class RUMFragmentTest {
     }
 
     @Test
-    fun fragmentLifecycle() {
+    fun activityLifecycle() {
+        val mockActivity = mock<AppCompatActivity>(AppCompatActivity::class.java)
         val mockFragmentManager = mock<FragmentManager>(FragmentManager::class.java)
+        whenever(mockActivity.supportFragmentManager).thenReturn(mockFragmentManager)
         val mockRUM = mock<RUM>(RUM::class.java)
         val rumFragment = RUMFragment(mockRUM)
-        val fragment = Fragment()
+        val rumActivity = RUMActivity(mockRUM, rumFragment)
 
         // Simulate lifecycle from Created to Destroyed
-        rumFragment.onFragmentCreated(mockFragmentManager, fragment, null)
-        rumFragment.onFragmentStarted(mockFragmentManager, fragment)
-        rumFragment.onFragmentResumed(mockFragmentManager, fragment)
-        rumFragment.onFragmentPaused(mockFragmentManager, fragment)
-        rumFragment.onFragmentStopped(mockFragmentManager, fragment)
-        rumFragment.onFragmentDestroyed(mockFragmentManager, fragment)
+        rumActivity.onActivityCreated(mockActivity, null)
+        rumActivity.onActivityStarted(mockActivity)
+        rumActivity.onActivityResumed(mockActivity)
+        rumActivity.onActivityPaused(mockActivity)
+        rumActivity.onActivityStopped(mockActivity)
+        rumActivity.onActivityDestroyed(mockActivity)
 
-        // onFragmentResumed should have called to sendRUMTimingEvent
+        // onActivityResumed should have called to sendRUMTimingEvent
         verify(mockRUM).sendRUMTimingEvent(
-            eq(RaygunRUMEventType.FRAGMENT_LOADED),
-            eq("Fragment"),
+            eq(RaygunRUMEventType.ACTIVITY_LOADED),
+            eq("AppCompatActivity"),
             anyLong()
         )
 
-        // seen should be called for each "onFragment..."
+        // seen should be called for each "onActivity..."
         verify(mockRUM, times(6)).seen()
     }
 }
