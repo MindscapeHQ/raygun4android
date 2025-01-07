06d14795364e5402f3c5248ddef073319212341b
Miguel Beltran <m@beltran.work>
2025-01-07 16:37:52 +0100

    add RUMFragmentTest

--- provider/src/test/java/com/raygun/raygun4android/rum/RUMFragmentTest.kt
+++ provider/src/test/java/com/raygun/raygun4android/rum/RUMFragmentTest.kt
@@ -4,6 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.raygun.raygun4android.RaygunRUMEventType
 import com.raygun.raygun4android.TestTree
+import junit.framework.TestCase.fail
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -13,6 +14,7 @@ import org.mockito.Mockito.anyLong
 import org.mockito.Mockito.mock
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.any
+import org.mockito.kotlin.eq
 import org.mockito.kotlin.verify
 import timber.log.Timber
 
@@ -37,8 +39,20 @@ class RUMFragmentTest {
         val mockRUM = mock<RUM>(RUM::class.java)
         val rumFragment = RUMFragment(mockRUM)
         val fragment = Fragment()
+
+        // Simulate lifecycle from Created to Destroyed
         rumFragment.onFragmentCreated(mockFragmentManager, fragment, null)
+        rumFragment.onFragmentStarted(mockFragmentManager, fragment)
         rumFragment.onFragmentResumed(mockFragmentManager, fragment)
-        verify(mockRUM).sendRUMTimingEvent(RaygunRUMEventType.FRAGMENT_LOADED, "Fragment", anyLong())
+        rumFragment.onFragmentPaused(mockFragmentManager, fragment)
+        rumFragment.onFragmentStopped(mockFragmentManager, fragment)
+        rumFragment.onFragmentDestroyed(mockFragmentManager, fragment)
+
+        // onFragmentResumed should have called to sendRUMTimingEvent
+        verify(mockRUM).sendRUMTimingEvent(
+            eq(RaygunRUMEventType.FRAGMENT_LOADED),
+            eq("Fragment"),
+            anyLong()
+        )
     }
 }
