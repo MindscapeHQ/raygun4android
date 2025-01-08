c7ff42b46113221997d59afaa60897702d4d184f
Miguel Beltran <m@beltran.work>
2025-01-08 11:50:43 +0100

    cleanup sample app

--- app/src/main/java/com/raygun/raygun4android/sample/fragments/NavigationActivity.kt
+++ app/src/main/java/com/raygun/raygun4android/sample/fragments/NavigationActivity.kt
@@ -22,6 +22,7 @@ class NavigationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_navigation)
+        setTitle(R.string.fragment_text)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
