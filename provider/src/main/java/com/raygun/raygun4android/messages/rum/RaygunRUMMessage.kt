b70881886f88fe5106f2551c0058673c3b349e0f
Miguel Beltran <m@beltran.work>
2025-04-14 11:02:03 +0200

    converted RaygunRUMData to Kotlin data classes

---
+++ provider/src/main/java/com/raygun/raygun4android/messages/rum/RaygunRUMMessage.kt
@@ -0,0 +1,18 @@
+package com.raygun.raygun4android.messages.rum
+
+data class RaygunRUMMessage(
+    var eventData: Array<RaygunRUMDataMessage> = emptyArray()
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as RaygunRUMMessage
+
+        return eventData.contentEquals(other.eventData)
+    }
+
+    override fun hashCode(): Int {
+        return eventData.contentHashCode()
+    }
+}
---
+++ provider/src/main/java/com/raygun/raygun4android/messages/rum/RaygunRUMTimingMessage.kt
@@ -0,0 +1,19 @@
+package com.raygun.raygun4android.messages.rum
+
+data class RaygunRUMTimingMessage(
+    val type: String,
+    val duration: Long,
+) {
+    class Builder(internal val type: String) {
+        private var duration: Long = 0
+
+        fun duration(duration: Long): Builder {
+            this.duration = duration
+            return this
+        }
+
+        fun build(): RaygunRUMTimingMessage {
+            return RaygunRUMTimingMessage(type, duration)
+        }
+    }
+}
