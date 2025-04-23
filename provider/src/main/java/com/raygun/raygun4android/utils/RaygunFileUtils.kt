063a6e11c2b9d0c54611264a914598c9f4d59f02
Miguel Beltran <m@beltran.work>
2025-04-23 11:00:19 +0200

    feat: convert utils to Kotlin

---
+++ provider/src/main/java/com/raygun/raygun4android/utils/RaygunFileUtils.kt
@@ -0,0 +1,47 @@
+package com.raygun.raygun4android.utils
+
+import android.content.Context
+import com.raygun.raygun4android.RaygunSettings
+import com.raygun.raygun4android.logging.RaygunLogger.e
+import com.raygun.raygun4android.logging.RaygunLogger.w
+import kotlin.math.max
+
+object RaygunFileUtils {
+    fun getExtension(filename: String?): String? {
+        if (filename == null) {
+            return null
+        }
+        val separator =
+            max(filename.lastIndexOf('/').toDouble(), filename.lastIndexOf('\\').toDouble()).toInt()
+        val dotPos = filename.lastIndexOf(".")
+        val index = if (separator > dotPos) -1 else dotPos
+        return if (index == -1) {
+            ""
+        } else {
+            filename.substring(index + 1)
+        }
+    }
+
+    @JvmStatic
+    fun clearCachedReports(context: Context) {
+        synchronized(this) {
+            val fileList = context.cacheDir.listFiles(RaygunFileFilter())
+            if (fileList != null) {
+                for (f in fileList) {
+                    if (getExtension(f.name)
+                            .equals(RaygunSettings.DEFAULT_FILE_EXTENSION, ignoreCase = true)
+                    ) {
+                        if (!f.delete()) {
+                            w("Couldn't delete cached report (" + f.name + ")")
+                        }
+                    }
+                }
+            } else {
+                e(
+                    "Error in handling cached message from filesystem - could not get a list of"
+                            + " files from cache dir"
+                )
+            }
+        }
+    }
+}
---
+++ provider/src/main/java/com/raygun/raygun4android/utils/RaygunUtils.kt
@@ -0,0 +1,41 @@
+package com.raygun.raygun4android.utils
+
+object RaygunUtils {
+    fun mergeLists(firstList: List<*>?, secondList: List<*>?): List<*> {
+        if (firstList == null && secondList == null) {
+            return emptyList<Any?>()
+        }
+
+        // If only the first list has contents return that.
+        if (firstList != null && secondList == null) {
+            return firstList
+        }
+
+        // If only the second list has contents return that.
+        if (firstList == null && secondList != null) {
+            return secondList
+        }
+
+        // Concatenate the two lists
+        return firstList!! + secondList!!
+    }
+
+    fun mergeMaps(firstMap: Map<*, *>?, secondMap: Map<*, *>?): Map<*, *> {
+        if (firstMap == null && secondMap == null) {
+            return emptyMap<Any?, Any?>()
+        }
+
+        // If only the first map has contents return that.
+        if (firstMap != null && secondMap == null) {
+            return firstMap
+        }
+
+        // If only the second map has contents return that.
+        if (firstMap == null && secondMap != null) {
+            return secondMap
+        }
+
+        // Merge the two maps, second map takes preference if there are duplicate keys.
+        return firstMap!! + secondMap!!
+    }
+}
